3cca5290a44ed9bd54043911f77cac1a19ef660e
Yu357 <forestgreen14689@gmail.com>
2022-01-06 14:38:35 +0900

    TODOコメント書いたりした

--- app/src/main/java/com/example/firetasknotebook/FirstFragment.kt
+++ app/src/main/java/com/example/firetasknotebook/FirstFragment.kt
@@ -12,8 +12,14 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //TODO: Firestoreのtodosコレクションを取得する
+        //TODO: RecyclerViewを表示する
     }
 
 }
