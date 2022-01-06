0eef12c7ee21a0d913685d949a16466e10032373
Yu357 <forestgreen14689@gmail.com>
2022-01-06 14:59:21 +0900

    todosコレクションのスナップショットをリッスンできた

--- app/src/main/java/com/example/firetasknotebook/FirstFragment.kt
+++ app/src/main/java/com/example/firetasknotebook/FirstFragment.kt
@@ -1,10 +1,13 @@
 package com.example.firetasknotebook
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FirstFragment : Fragment() {
 
@@ -19,6 +22,26 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //TODO: Firestoreのtodosコレクションを取得する
+
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("todos").addSnapshotListener { value, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed!", e)
+                return@addSnapshotListener
+            }
+
+            val contents = ArrayList<String>()
+            for (doc in value!!) {
+                doc.getString("content")?.let {
+                    contents.add(it)
+                }
+            }
+            Log.d(TAG, "todo contents: $contents")
+        }
+
+
+
         //TODO: RecyclerViewを表示する
     }
 
