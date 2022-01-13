c2e466d77d68095b3b27e3a345e347468a434526
Yu357 <forestgreen14689@gmail.com>
2022-01-13 15:38:09 +0900

    リファクタリング

--- app/src/main/java/com/example/firetasknotebook/EditActivity.kt
+++ app/src/main/java/com/example/firetasknotebook/EditActivity.kt
@@ -20,10 +20,7 @@ class EditActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit)
 
         //既存Todo編集時は、編集対象ドキュメントのidを取得
-        val extraValue = intent.getStringExtra("id")
-        if(extraValue != null) {
-            id = extraValue
-        }
+        id = intent.getStringExtra(id)
 
         //既存Todo更新時
         if(id != null){
