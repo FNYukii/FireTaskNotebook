ee86c0adfdd88cd43b1b5bc803207f2840b61fc1
Yu357 <forestgreen14689@gmail.com>
2022-01-14 18:10:58 +0900

    デフォルトのRecyclerViewAdapterに戻した

--- app/src/main/java/com/example/firetasknotebook/FirstFragment.kt
+++ app/src/main/java/com/example/firetasknotebook/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.example.firetasknotebook
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.fragment_first.*
@@ -29,46 +31,27 @@ class FirstFragment : Fragment() {
             startActivity(intent)
         }
 
-        //todosコレクションのスナップショットを取得して表示
-//        val db = FirebaseFirestore.getInstance()
-//        db.collection("todos")
-//            .whereEqualTo("isAchieved", false)
-//            .orderBy("created_at", Query.Direction.DESCENDING)
-//            .addSnapshotListener { value, e ->
-//            if (e != null) {
-//                Log.w(TAG, "Snapshotリッスン失敗!", e)
-//                return@addSnapshotListener
-//            }
-//
-//            val todos = ArrayList<Todo>()
-//            for (doc in value!!) {
-//                val id = doc.id
-//                val content = doc.get("content").toString()
-//                todos.add(Todo(id, content))
-//            }
-//
-//            //TODO: RecyclerViewを自動更新する
-//            recyclerView01.adapter = TodoRecyclerAdapter(todos)
-//            recyclerView01.layoutManager = LinearLayoutManager(this.context)
-//        }
-
-
-
-
+        //RecyclerView with Cloud Firestore
         val db = FirebaseFirestore.getInstance()
-        val query = db.collection("todos")
+        db.collection("todos")
             .whereEqualTo("isAchieved", false)
             .orderBy("created_at", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, e ->
+            if (e != null) {
+                Log.w(TAG, "Snapshotリッスン失敗!", e)
+                return@addSnapshotListener
+            }
 
+            val todos = ArrayList<Todo>()
+            for (doc in value!!) {
+                val id = doc.id
+                val content = doc.get("content").toString()
+                todos.add(Todo(id, content))
+            }
 
-
-        val options = FirestoreRecyclerOptions.Builder<Todo>()
-            .setQuery(query, Todo::class.java)
-            .build()
-
-        recyclerView01.adapter = TodoFirestoreRecyclerAdapter(options)
-
-
-
+            //TODO: RecyclerViewを自動更新する
+            recyclerView01.adapter = TodoRecyclerViewAdapter(todos)
+            recyclerView01.layoutManager = LinearLayoutManager(this.context)
+        }
     }
 }
---
+++ app/src/main/java/com/example/firetasknotebook/TodoRecyclerViewAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.firetasknotebook
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoViewHolder (item: View) : RecyclerView.ViewHolder(item) {
+    val contentText: TextView = item.findViewById(R.id.contentText)
+}
+
+class TodoRecyclerViewAdapter(private val todos: ArrayList<Todo>) : RecyclerView.Adapter<TodoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.one_todo, parent, false)
+        return TodoViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.contentText.text = todos[position].content
+        holder.itemView.setOnClickListener {
+            val intent = Intent(it.context, EditActivity::class.java)
+            intent.putExtra("id", todos[position].id)
+            it.context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int = todos.size
+}
